6b0cd86fa52a0bc607bd28a1c89d7fe743e0545f
root <ionut.pirva@gmail.com>
2019-02-08 00:17:51 +0000

    New

--- ansible/README.md
+++ ansible/README.md
@@ -1,1 +1,87 @@
 nexus9000 with ansible - vxlan evpn underlay and overlay infrastructure
+
+Fabric 1
+#SN:SAL1830XABP -- n9k-spine-1
+
+#SN:FDO20511MS6 -- n9k-leaf-1
+#SN:FDO20441PZS -- n9k-leaf-2
+#SN:FDO20400MVA -- n9k-leaf-3
+#SN:FDO21050FUA -- n9k-leaf-4
+
+#SN:FDO20371M2Z -- n9k-bleaf-1
+#SN:FDO211120Q0 -- n9k-bgw-1 (Multi-Site)
+
+Bring up Fabric 1
+(~/ansible/fabric1)-(22 files, 100Kb)--> date; ansible-playbook -i /root/ansible/scripts/inventory.py ___bring_up_fabric.yml; date
+Thu Sep 27 21:26:04 UTC 2018
+PLAY RECAP ***************************************************************************************************************************************************
+n9k-bgw-1                  : ok=68   changed=48   unreachable=0    failed=0
+n9k-bleaf-1                : ok=78   changed=41   unreachable=0    failed=0
+n9k-leaf-1                 : ok=76   changed=53   unreachable=0    failed=0
+n9k-leaf-2                 : ok=76   changed=53   unreachable=0    failed=0
+n9k-leaf-3                 : ok=76   changed=53   unreachable=0    failed=0
+n9k-leaf-4                 : ok=76   changed=53   unreachable=0    failed=0
+n9k-spine-1                : ok=40   changed=31   unreachable=0    failed=0
+Thu Sep 27 21:31:49 UTC 2018
+
+
+Fabric 2
+#SN:FDO21201JVG -- n9k-spine-2
+#SN:FDO210627TL -- n9k-leaf-5
+
+#SN:FDO20370B9Q -- n9k-bleaf-3
+
+#SN:FDO20340P41 -- n9k-bgw-2 (Multi-Site)
+#SN:FDO210507CS -- n9k-bgw-3 (Multi-Site)
+
+Bring up Fabric 2
+(~/ansible/fabric2)-(22 files, 96Kb)--> date; ansible-playbook -i /root/ansible/scripts/inventory.py ___bring_up_fabric.yml;date
+Thu Sep 27 21:41:16 UTC 2018
+PLAY RECAP ***************************************************************************************************************************************************
+n9k-bgw-2                  : ok=72   changed=52   unreachable=0    failed=0
+n9k-bgw-3                  : ok=72   changed=52   unreachable=0    failed=0
+n9k-bleaf-3                : ok=81   changed=44   unreachable=0    failed=0
+n9k-leaf-5                 : ok=76   changed=39   unreachable=0    failed=0
+n9k-spine-2                : ok=40   changed=31   unreachable=0    failed=0
+
+Thu Sep 27 21:45:15 UTC 2018
+
+
+export NXAPI_PSW=""
+export SPARK_ROOM_ID=""
+export SPARK_TOKEN=""
+
+cd /tftpboot/scripts/tshoot/check_cabling/ && python /tftpboot/scripts/tshoot/check_cabling/checkcable.py SAL1830XABP /root/ansible/fabric1/connectivity.json lldp
+
+
+/root/ansible/scripts/inventory.py --topology_file=/root/ansible/fabric1/topology.yml --connectivity_file=/root/ansible/fabric1/connectivity.json --write
+
+Get facts:
+ansible-playbook -i /root/ansible/scripts/inventory.py  check_connectivity.yml
+
+Generate needed variables (underlay,overlay, vpc ...):
+ansible-playbook -i /root/ansible/scripts/inventory.py generate_variables.yml
+ansible-playbook -i /root/ansible/scripts/inventory.py generate_vpc_variables.yml
+
+Activate needed features:
+ansible-playbook -i /root/ansible/scripts/inventory.py configure_leaf_features.yml
+ansible-playbook -i /root/ansible/scripts/inventory.py configure_spine_features.yml
+
+Configure Underlay:
+ansible-playbook -i /root/ansible/scripts/inventory.py configure_underlay.yml
+
+Configure Overlay:
+ansible-playbook -i /root/ansible/scripts/inventory.py configure_overlay.yml
+
+Configure VPC:
+ansible-playbook -i /root/ansible/scripts/inventory.py configure_vpc.yml
+
+Save & Backup:
+ansible-playbook -i /root/ansible/scripts/inventory.py save_and_backup_configuration.yml
+
+Fabric2
+
+cd /tftpboot/scripts/tshoot/check_cabling/ && python /tftpboot/scripts/tshoot/check_cabling/checkcable.py FDO21201JVG /root/ansible/fabric2/connectivity.json lldp
+
+/root/ansible/scripts/inventory.py --topology_file=/root/ansible/fabric2/topology.yml --connectivity_file=/root/ansible/fabric2/connectivity.json --write
+
--- ansible/scripts/inventory.py
+++ ansible/scripts/inventory.py
@@ -12,13 +12,23 @@ import json
 import yaml
 import re
 import sys
+import os
 
+# current directory
+wdir = os.getcwd()
+ 
 if len(sys.argv) <= 1:
     help = """
 Please use one of the following arguments:
---list - inventory list
+--list - inventory list (Ansible uses this option by default)
 --topology - show topology details
 --write - write topology details on disk
+and specify the following parameters:
+--topology_file - topology.yml file to which the script will write the topology information
+--connectivity_file - connectivity.json file where you described the fabric connectivity
+
+If no --topology_file specified, the script tries to load topology.yml from the current working directory.
+If no --connectivity_file specified, the script tries to load connectivity.json from the current working directory.
 """
     print help
     exit()
@@ -26,15 +36,79 @@ Please use one of the following arguments:
 show_topology = 0
 write_topology = 0
 
+topology_file = ""
+connectivity_file = ""
+
 for i in range(0,len(sys.argv)):
-    if sys.argv[i] == "--topology":
+    try:
+        sys.argv[i].split("=")[1]
+    except:
+        # do nothing
+        arg = ""
+        value = ""
+    else:
+        arg = sys.argv[i].split("=")[0]
+        value = sys.argv[i].split("=")[1]
+    if arg == "--topology":
         show_topology = 1
     if sys.argv[i] == "--write":
         write_topology = 1
+    if arg == "--topology_file":
+       topology_file = value
+    if arg == "--connectivity_file":
+       connectivity_file = value
+# --topology_file is generated from --connectivity_file
+# if no --topology_file specified and topology.yml exists in the current working directory => stop, error
+# same for --connectivity_file
 
-connectivity="/tftpboot/scripts/tshoot/check_cabling/connectivity.json"
-topology_file="/root/ansible/fabric/topology.yml"
-groups = ("spine","leaf","bleaf")
+if len(topology_file) == 0:
+    try:
+        open(wdir+"/topology.yml", "r")
+    except:
+        print "--topology_file not specified and no topology.yml found in the current working directory"
+        exit()
+    else:
+        topology_file = wdir+"/topology.yml"
+
+if len(connectivity_file) == 0:
+    try:
+        open(wdir+"/connectivity.json", "r")
+    except:
+        print "--connectivity_file not specified and no connectivity.json found in the current working directory"
+        exit()
+    else:
+        connectivity_file = wdir+"/connectivity.json"
+
+if len(topology_file) == 0 or len(connectivity_file) == 0:
+    print "--topology_file and --connectivity_file must be specified."     
+    exit()
+
+try:
+    dir = os.path.dirname(topology_file)
+except Exception as e:
+    print "Check --topology_file {}".format(str(e))
+    exit()
+
+try:
+    open(topology_file, "a")
+except Exception as e:
+    print "Check --topology_file {}".format(str(e))
+    exit()
+
+try:
+    open(connectivity_file, "r")
+except Exception as e:
+    print "Check --connectivity_file {}".format(str(e))
+    exit()
+
+#connectivity="/tftpboot/scripts/tshoot/check_cabling/connectivity.json"
+#topology_file="/root/ansible/fabric/topology.yml"
+
+groups = ("spine","leaf","bleaf","bgw")
+# wan routers have in the name
+look = "wan"
+
+connectivity = connectivity_file
 
 with open(connectivity, 'r') as connectivity_file:
     conn_db = connectivity_file.read()
@@ -43,6 +117,9 @@ conn_db_nodes = dict()
 for i in groups:
     conn_db_nodes[i] = list()
 nes = conn_obj["ne"]
+# print(nes)
+# [{u'connectivity': [{u'local_ifx': u'mgmt0', u'remote_ne': u'SW-CORP.priv.gsp-france-lab.com', u'remote_ifx': u'Eth103/1/5'}, {u'local_ifx': u'Eth1/31', u'remote_ne': u'n9k-leaf-5', u'remote_ifx': u'Ethernet1/49'}, {u'local_ifx': u'Eth1/32', u'remote_ne': u'n9k-bleaf-3', u'remote_ifx': u'Ethernet1/49'}, {u'local_ifx': u'Eth1/33', u'remote_ne': u'n9k-bgw-3', u'remote_ifx': u'Ethernet1/49'}, {u'local_ifx': u'Eth1/34', u'remote_ne': u'n9k-bgw-4', u'remote_ifx': u'Ethernet1/49'}], u'name': u'n9k-spine-2'}, {u'connectivity': [{u'local_ifx': u'mgmt0', u'remote_ne': u'SW-CORP.priv.gsp-france-lab.com', u'remote_ifx': u'Eth103/1/6'}, {u'local_ifx': u'Eth1/49', u'remote_ne': u'n9k-spine-2', u'remote_ifx': u'Ethernet1/31'}], u'name': u'n9k-leaf-5'}, {u'connectivity': [{u'local_ifx': u'mgmt0', u'remote_ne': u'SW-CORP.priv.gsp-france-lab.com', u'remote_ifx': u'Eth103/1/7'}, {u'local_ifx': u'Eth1/49', u'remote_ne': u'n9k-spine-2', u'remote_ifx': u'Ethernet1/32'}], u'name': u'n9k-bleaf-3'}, {u'connectivity': [{u'local_ifx': u'mgmt0', u'remote_ne': u'SW-CORP.priv.gsp-france-lab.com', u'remote_ifx': u'Eth103/1/9'}, {u'local_ifx': u'Eth1/49', u'remote_ne': u'n9k-spine-2', u'remote_ifx': u'Ethernet1/33'}, {u'local_ifx': u'Eth1/20', u'remote_ne': u'wan', u'remote_ifx': u'Ethernet1/4'}], u'name': u'n9k-bgw-3'}, {u'connectivity': [{u'local_ifx': u'mgmt0', u'remote_ne': u'SW-CORP.priv.gsp-france-lab.com', u'remote_ifx': u'Eth103/1/8'}, {u'local_ifx': u'Eth1/49', u'remote_ne': u'n9k-spine-2', u'remote_ifx': u'Ethernet1/34'}, {u'local_ifx': u'Eth1/20', u'remote_ne': u'wan', u'remote_ifx': u'Ethernet1/3'}], u'name': u'n9k-bgw-4'}]
+
 for i in range(0, len(nes)):
     ne_name = conn_obj["ne"][i]["name"]
     for type in groups:
@@ -67,6 +144,8 @@ for type in groups:
         data[type]["vars"] = dict()
     for node in conn_db_nodes[type]:
         data[type]["hosts"].append(node)
+
+# this is needed when the script is called from ansible-playbook -i inventory.py
 print json.dumps(data,indent=4)
 
 '''
@@ -94,7 +173,7 @@ for i in range(0, len(nes)):
     ne_connectivity = conn_obj["ne"][i]["connectivity"]
     if ne_name in topo:
         n = 0
-        if ne_name in conn_db_nodes["leaf"] or ne_name in conn_db_nodes["bleaf"]:
+        if ne_name in conn_db_nodes["leaf"] or ne_name in conn_db_nodes["bleaf"] or ne_name in conn_db_nodes["bgw"]:
             for j in conn_db_nodes["spine"]:
                 for k in range(0, len(ne_connectivity)):
                     if ne_connectivity[k]["remote_ne"] == j:
@@ -104,6 +183,7 @@ for i in range(0, len(nes)):
                         topo[ne_name][n]["linkid"]=""
 			topo[ne_name][n]["portid"]=""
                         topo[ne_name][n]["peer"]=ne_connectivity[k]["remote_ne"]
+                        topo[ne_name][n]["linktype"]="fabric"
                         n+=1
         if ne_name in conn_db_nodes["spine"]:
             for j in conn_db_nodes["leaf"]:
@@ -126,6 +206,56 @@ for i in range(0, len(nes)):
 			topo[ne_name][n]["portid"]=""
                         topo[ne_name][n]["peer"]=ne_connectivity[k]["remote_ne"]
                         n+=1
+            for j in conn_db_nodes["bgw"]:
+                for k in range(0, len(ne_connectivity)):
+                    if ne_connectivity[k]["remote_ne"] == j:
+                        topo[ne_name][n]=dict()
+                        topo[ne_name][n]["local_ifx"]=ne_connectivity[k]["local_ifx"]
+                        topo[ne_name][n]["remote_ifx"]=ne_connectivity[k]["remote_ifx"]
+                        topo[ne_name][n]["linkid"]=""
+                        topo[ne_name][n]["portid"]=""
+                        topo[ne_name][n]["peer"]=ne_connectivity[k]["remote_ne"]
+                        n+=1
+# topo dict
+# {u'n9k-bgw-3': {0: {'peer': u'n9k-spine-2', 'linkid': '', 'local_ifx': u'Eth1/49', 'portid': '', 'remote_ifx': u'Ethernet1/33'}}, u'n9k-bgw-4': {0: {'peer': u'n9k-spine-2', 'linkid': '', 'local_ifx': u'Eth1/49', 'portid': '', 'remote_ifx': u'Ethernet1/34'}}, u'n9k-leaf-5': {0: {'peer': u'n9k-spine-2', 'linkid': '', 'local_ifx': u'Eth1/49', 'portid': '', 'remote_ifx': u'Ethernet1/31'}}, u'n9k-bleaf-3': {0: {'peer': u'n9k-spine-2', 'linkid': '', 'local_ifx': u'Eth1/49', 'portid': '', 'remote_ifx': u'Ethernet1/32'}}, u'n9k-spine-2': {0: {'peer': u'n9k-leaf-5', 'linkid': '', 'local_ifx': u'Eth1/31', 'portid': '', 'remote_ifx': u'Ethernet1/49'}, 1: {'peer': u'n9k-bleaf-3', 'linkid': '', 'local_ifx': u'Eth1/32', 'portid': '', 'remote_ifx': u'Ethernet1/49'}, 2: {'peer': u'n9k-bgw-3', 'linkid': '', 'local_ifx': u'Eth1/33', 'portid': '', 'remote_ifx': u'Ethernet1/49'}, 3: {'peer': u'n9k-bgw-4', 'linkid': '', 'local_ifx': u'Eth1/34', 'portid': '', 'remote_ifx': u'Ethernet1/49'}}}
+
+'''
+read topology and determine fabric and dci links for the bgw, update topo
+'''
+bgw = dict()
+bgw["tracking"] = dict()
+bgw["tracking"]["fabric"] = dict()
+
+bgw["tracking"]["dci"] = dict()
+
+for bgw_node in topo:
+    if re.match('.*-bgw-.*',bgw_node,re.IGNORECASE):
+        topo_bgw = topo[bgw_node]
+        #print(topo_bgw)
+        bgw["tracking"]["fabric"][bgw_node] = dict()
+        bgw["tracking"]["dci"][bgw_node] = dict()
+
+        for i in range(0, len(nes)):
+            ne_name = conn_obj["ne"][i]["name"]
+            ne_connectivity = conn_obj["ne"][i]["connectivity"]
+            if ne_name == bgw_node:
+                n = 0
+                for k in range(0, len(ne_connectivity)):
+                    remote_ne = ne_connectivity[k]["remote_ne"]
+                    if re.match('.*'+look+'.*',remote_ne,re.IGNORECASE):
+                        bgw["tracking"]["dci"][bgw_node][n] = dict()
+                        bgw["tracking"]["dci"][bgw_node][n]["local_ifx"] = ne_connectivity[k]["local_ifx"]
+                        bgw["tracking"]["dci"][bgw_node][n]["remote_ifx"] = ne_connectivity[k]["remote_ifx"]
+                        bgw["tracking"]["dci"][bgw_node][n]["peer"] = remote_ne
+                        n+=1
+                    else:
+                        if remote_ne in topo:
+                            bgw["tracking"]["fabric"][bgw_node][n] = dict()
+                            bgw["tracking"]["fabric"][bgw_node][n]["local_ifx"] = ne_connectivity[k]["local_ifx"]
+                            bgw["tracking"]["fabric"][bgw_node][n]["remote_ifx"] = ne_connectivity[k]["remote_ifx"]
+                            bgw["tracking"]["fabric"][bgw_node][n]["peer"] = remote_ne
+                            n+=1
+
 ''' 
 read topology and determine linkid and portid
 start from spine / spines
@@ -220,6 +350,7 @@ if write_topology == 1:
             yaml.safe_dump(nodes, yaml_file, default_flow_style=False)
             yaml.safe_dump(vpc_pairs, yaml_file, default_flow_style=False)
             yaml.safe_dump(vpc_interfaces, yaml_file, default_flow_style=False)
+            yaml.safe_dump(bgw, yaml_file, default_flow_style=False)
             yaml.safe_dump(topology, yaml_file, default_flow_style=False)
     except:
         print ""
@@ -233,4 +364,5 @@ if show_topology ==1:
     print yaml.safe_dump(nodes, default_flow_style=False)  
     print yaml.safe_dump(vpc_pairs, default_flow_style=False)
     print yaml.safe_dump(vpc_interfaces, default_flow_style=False)
+    print yaml.safe_dump(bgw, default_flow_style=False)
     print yaml.safe_dump(topology, default_flow_style=False)
